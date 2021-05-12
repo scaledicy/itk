afd18d12748717412953d689eeacc081f06ebfef
Vadym Liesin <scaledicy@gmail.com>
2021-05-13 01:51:33 +0300

    Remove unused scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import CssBaseline from "@material-ui/core/CssBaseline";
-import s from "./App.module.scss";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Aside from "./components/Aside/Aside";
@@ -12,16 +11,16 @@ const App = props => {
     <>
       <CssBaseline />
 
-      <div className={s.appContainer}>
+      <div className='appContainer'>
         <Header />
-        <div className={s.appGridContent}>
+        <div className='appGridContent'>
           <Aside friends={props.appState.friends} />
-          <div className={s.appContent}>
+          <div className='appContent'>
             <Switch>
               <Route path='/profile'>
                 <Profile posts={props.appState.profilePage.posts} />
               </Route>
-              <Redirect from="/" to="/profile" />
+              <Redirect from='/' to='/profile' />
             </Switch>
             <Route exact path='/messages'>
               <Dialogs messagesPage={props.appState.messagesPage} />

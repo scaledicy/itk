2dcad34cfc2a2b233a16f4a30ce4ae2727895538
dev1 <dev1@gmail.com>
2021-05-26 13:07:04 +0300

    L47 add copy state for profile and dialogs reducers

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,7 +2,7 @@ import s from "./Profile.module.scss";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
 
-const Profile = props => {
+const Profile = () => {
   return (
     <>
       <h1 className={s.profileTitle}>Profile</h1>
--- src/index.js
+++ src/index.js
@@ -7,24 +7,16 @@ import { BrowserRouter as Router } from "react-router-dom";
 import store from "./redux/ReduxStore";
 import { Provider } from "react-redux";
 
-let rerenderEntireTree = () => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Router>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </Router>
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-};
-
-rerenderEntireTree();
-
-store.subscribe(() => {
-  rerenderEntireTree();
-});
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

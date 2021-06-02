84baaaa908ddfb4cf8ef399656606f86c8f2a122
dev1 <dev1@gmail.com>
2021-06-02 12:08:49 +0300

    L56 split users into presentation and container components

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,8 +1,7 @@
-import s from "./Users.module.scss";
-import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
-import * as axios from "axios";
 import userEmpty from "../../assets/images/user-empty.svg";
+import { makeStyles } from "@material-ui/core/styles";
+import s from "./Users.module.scss";
 
 const useStyles = makeStyles({
     btnFollow: {
@@ -14,72 +13,97 @@ const useStyles = makeStyles({
 });
 
 const Users = props => {
-    let getUsers = () => {
-        if (props.users.length === 0) {
-            axios
-                .get("https://social-network.samuraijs.com/api/1.0/users")
-                .then(response => {
-                    props.setUsers(response.data.items);
-                });
-        }
-    };
+    const classes = useStyles();
+    let pageCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
-    const classes = useStyles();   
-
-    let user = props.users.map(u => {
-        return (
-            <div className={s.userItem} key={u.id} id={u.id}>
-                {u.photos.small !== null ? (
-                    <img
-                        className={s.userPhoto}
-                        src={u.photos.small}
-                        alt='userPhoto'
-                    />
-                ) : (
-                    <img
-                        className={s.userEmpty}
-                        src={userEmpty}
-                        alt='emptyPhoto'
-                    />
-                )}
-                <div className={s.numberOfUser}>{u.id}</div>
-                <div className={s.userDesc}>
-                    <h1>{u.name}</h1>
-                    <div className={s.subStatus}>
-                        Subscription status: &nbsp;
-                        <span className={u.followed ? s.followed : null}>
-                            {u.followed ? "Followed" : "Unfollowed"}
-                        </span>
-                    </div>
-                    <div className={s.statusMessage}>Status: {u.status}</div>
-                    {u.followed ? (
-                        <Button
-                            onClick={() => props.unFollow(u.id)}
-                            className={classes.btnFollow}
-                            variant='contained'
-                            color='secondary'
-                        >
-                            Unfollow
-                        </Button>
-                    ) : (
-                        <Button
-                            onClick={() => props.follow(u.id)}
-                            className={classes.btnFollow}
-                            variant='contained'
-                            color='primary'
-                        >
-                            Follow
-                        </Button>
-                    )}
-                </div>
-            </div>
-        );
-    });
+    let pages = [];
+    for (let i = 1; i <= pageCount; i++) {
+        pages.push(i);
+    }
     return (
         <div className={s.usersContainer}>
             <h1 className={s.usersTitle}>Users list</h1>
-            <button type="button" onClick={getUsers}>Get users</button>
-            <div className={s.usersList}>{user}</div>
+            <div className={s.paginationContainer}>
+                <Button variant='contained' color='primary'>
+                    Prev
+                </Button>
+                <ul>
+                    {pages.map(p => {
+                        return (
+                            <li
+                                className={
+                                    props.currentPage === p && s.selected
+                                }
+                                onClick={e => {
+                                    props.onPageChanged(p);
+                                }}
+                            >
+                                {p}
+                            </li>
+                        );
+                    })}
+                </ul>
+                <Button variant='contained' color='primary'>
+                    Next
+                </Button>
+            </div>
+            <div className={s.usersList}>
+                {props.users.map((u, i) => {
+                    return (
+                        <div className={s.userItem} key={i} id={u.id}>
+                            {u.photos.small !== null ? (
+                                <img
+                                    className={s.userPhoto}
+                                    src={u.photos.small}
+                                    alt='userPhoto'
+                                />
+                            ) : (
+                                <img
+                                    className={s.userEmpty}
+                                    src={userEmpty}
+                                    alt='emptyPhoto'
+                                />
+                            )}
+                            <div className={s.numberOfUser}>{u.id}</div>
+                            <div className={s.userDesc}>
+                                <h1>{u.name}</h1>
+                                <div className={s.subStatus}>
+                                    Subscription status: &nbsp;
+                                    <span
+                                        className={
+                                            u.followed ? s.followed : null
+                                        }
+                                    >
+                                        {u.followed ? "Followed" : "Unfollowed"}
+                                    </span>
+                                </div>
+                                <div className={s.statusMessage}>
+                                    Status: {u.status}
+                                </div>
+                                {u.followed ? (
+                                    <Button
+                                        onClick={() => props.unFollow(u.id)}
+                                        className={classes.btnFollow}
+                                        variant='contained'
+                                        color='secondary'
+                                    >
+                                        Unfollow
+                                    </Button>
+                                ) : (
+                                    <Button
+                                        onClick={() => props.follow(u.id)}
+                                        className={classes.btnFollow}
+                                        variant='contained'
+                                        color='primary'
+                                    >
+                                        Follow
+                                    </Button>
+                                )}
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 };
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -6,7 +6,46 @@ import {
     setUsersAC,
     unfollowAC,
 } from "../../redux/UsersReducer";
-import UsersC from "./UsersC";
+import axios from "axios";
+import React from "react";
+import Users from "./Users";
+
+class UsersContainer extends React.Component {
+    componentDidMount() {
+        axios
+            .get(
+                `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+            )
+            .then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            });
+    }
+    onPageChanged = pageNumber => {
+        this.props.setCurrentPage(pageNumber);
+        axios
+            .get(
+                `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
+            )
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
+    };
+
+    render() {
+        return (
+            <Users
+                totalUsersCount={this.props.totalUsersCount}
+                pageSize={this.props.pageSize}
+                currentPage={this.props.currentPage}
+                onPageChanged={this.onPageChanged}
+                users={this.props.users}
+                follow={this.props.follow}
+                unfollow={this.props.unfollow}
+            />
+        );
+    }
+}
 
 let mapStateToProps = state => {
     return {
@@ -36,5 +75,4 @@ let mapDispatchToProps = dispatch => {
     };
 };
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersC);
-export default UsersContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);

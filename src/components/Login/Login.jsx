191336b418b2f940cfcbbec83ac0c9474ebb0737
dev1 <dev1@gmail.com>
2021-06-11 12:59:49 +0300

    Setup formik, test login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,38 @@
+import { useFormik } from "formik";
+
 const Login = () => {
-    return <h1>Login</h1>;
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+            console.log(values);
+        },
+    });
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <label htmlFor='email'>Email Address</label>
+            <input
+                id='email'
+                name='email'
+                type='email'
+                onChange={formik.handleChange}
+                value={formik.values.email}
+            />
+            <label htmlFor='password'>Password</label>
+            <input
+                id='password'
+                name='password'
+                type='password'
+                onChange={formik.handleChange}
+                value={formik.values.password}
+            />
+
+            <button type='submit'>Submit</button>
+        </form>
+    );
 };
 
 export default Login;

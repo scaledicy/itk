e47988d448fda6d14df241b7d153625bbc24bc66
dev1 <dev1@gmail.com>
2021-06-16 14:53:36 +0300

    L89 jest tests, remove React imports

--- src/redux/ProfileReducer.js
+++ src/redux/ProfileReducer.js
@@ -3,6 +3,7 @@ import { profileAPI } from "api/api";
 const ADD_POST = "ADD_POST";
 const SET_USER_PROFILE = "SET_USER_PROFILE";
 const SET_STATUS = "SET_STATUS";
+const DELETE_POST = "DELETE_POST";
 
 let initialState = {
     posts: [
@@ -40,6 +41,12 @@ const profileReducer = (state = initialState, action) => {
                 status: action.status,
             };
         }
+        case DELETE_POST: {
+            return {
+                ...state,
+                posts: state.posts.filter(p => p.id !== action.postId),
+            };
+        }
         default:
             return state;
     }
@@ -56,6 +63,10 @@ export const setUserProfile = profile => ({
 export const setStatus = status => ({
     type: SET_STATUS,
     status,
+});
+export const deletePost = postId => ({
+    type: DELETE_POST,
+    postId,
 });
 
 //Thunk actions creators
---
+++ src/redux/ProfileReducer.test.js
@@ -0,0 +1,35 @@
+import profileReducer, { addPost, deletePost } from "./ProfileReducer";
+
+let state = {
+    posts: [
+        { id: 1, message: "Hi, how are you?", likes: 12 },
+        { id: 2, message: "It's my first post", likes: 11 },
+    ],
+};
+
+test("length of posts should be incremented", () => {
+    //1. test data
+    let actionC = addPost("it-kamas.com");
+    //2.action
+    let newState = profileReducer(state, actionC);
+    //3.expectation
+    expect(newState.posts.length).toBe(3);
+});
+
+test("message of new post should be correct", () => {
+    //1. test data
+    let actionC = addPost("it-kamas.com");
+    //2.action
+    let newState = profileReducer(state, actionC);
+    //3.expectation
+    expect(newState.posts[2].message).toBe("it-kamas.com");
+});
+
+test("after deleting length of messages should be decrement", () => {
+    //1. test data
+    let actionC = deletePost(1);
+    //2.action
+    let newState = profileReducer(state, actionC);
+    //3.expectation
+    expect(newState.posts.length).toBe(1);
+});

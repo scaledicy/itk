39659ed5395329de4ead853f049c4cac6577f659
dev1 <dev1@gmail.com>
2021-06-25 14:12:42 +0300

    Create useProfileContainer hook

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,55 +1,19 @@
-import React, { useCallback, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import {
-    fetchUserProfile,
-    getStatus,
-    savePhoto,
-    updateStatus,
-} from "redux/ProfileReducer";
-import Profile from "./Profile";
+import React from 'react'
+import Profile from './Profile'
+import useProfileContainer from './useProfileContainer'
 
-const ProfileContainerF = () => {
-    const params = useParams();
-    const dispatch = useDispatch();
-
-    //Map dispatch to props
-    const data = useSelector(state => {
-        return {
-            profile: state.profilePage.profile,
-            status: state.profilePage.status,
-            authorizedUserId: state.auth.userId,
-        };
-    });
-
-    const userId = params.userId || data.authorizedUserId;
-
-    //Dispatch handlers
-    const fetchUserProfileHandler = useCallback(
-        userId => dispatch(fetchUserProfile(userId)),
-        [dispatch]
-    );
-    const savePhotoHandler = file => dispatch(savePhoto(file));
-    const getStatusHandler = useCallback(
-        userId => dispatch(getStatus(userId)),
-        [dispatch]
-    );
-    const updateStatusHandler = status => dispatch(updateStatus(status));
-
-    useEffect(() => {
-        fetchUserProfileHandler(userId);
-        getStatusHandler(userId);
-    }, [userId, fetchUserProfileHandler, getStatusHandler]);
+const ProfileContainer = () => {
+    const { data, handlers } = useProfileContainer()
 
     return (
         <Profile
             profile={data.profile}
             status={data.status}
-            updateStatus={updateStatusHandler}
-            isOwner={!params.userId}
-            savePhoto={savePhotoHandler}
+            updateStatus={handlers.updateStatus}
+            isOwner={data.isOwner}
+            savePhoto={handlers.savePhoto}
         />
-    );
-};
+    )
+}
 
-export default ProfileContainerF;
+export default ProfileContainer
---
+++ src/components/Profile/useProfileContainer.js
@@ -0,0 +1,64 @@
+import { useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { useCallback, useEffect } from 'react'
+import {
+    fetchUserProfile,
+    getStatus,
+    savePhoto,
+    updateStatus,
+} from 'redux/ProfileReducer'
+
+const useProfileContainer = () => {
+    const params = useParams()
+    const dispatch = useDispatch()
+
+    //==== Data from state ====
+    const data = useSelector(state => {
+        return {
+            profile: state.profilePage.profile,
+            status: state.profilePage.status,
+            authorizedUserId: state.auth.userId,
+        }
+    })
+
+    const userId = params.userId || data.authorizedUserId
+
+    //Dispatch handlers
+    const fetchUserProfileHandler = useCallback(
+        userId => dispatch(fetchUserProfile(userId)),
+        [dispatch]
+    )
+    const savePhotoHandler = useCallback(
+        file => dispatch(savePhoto(file)),
+        [dispatch]
+    )
+    const getStatusHandler = useCallback(
+        userId => dispatch(getStatus(userId)),
+        [dispatch]
+    )
+    const updateStatusHandler = useCallback(
+        status => dispatch(updateStatus(status)),
+        [dispatch]
+    )
+
+    useEffect(() => {
+        if (userId) {
+            fetchUserProfileHandler(userId)
+            getStatusHandler(userId)
+        }
+    }, [userId, fetchUserProfileHandler, getStatusHandler])
+
+    return {
+        data: {
+            profile: data.profile,
+            status: data.status,
+            isOwner: !params.userId,
+        },
+        handlers: {
+            updateStatus: updateStatusHandler,
+            savePhoto: savePhotoHandler,
+        },
+    }
+}
+
+export default useProfileContainer
